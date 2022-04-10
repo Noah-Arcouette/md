ee4bec21fc8119d34cc603e8e62a37917290fdba
Noah Arcouette <kcu9626@gmail.com>
2022-04-10 15:01:53 -0600

    add noadvo supported styles

---
+++ styles/doxy-noadvo.h
@@ -0,0 +1,40 @@
+/*
+	      _____           ___
+	     /  _  \         /  /
+	    /  / \  \       /  /
+	   /  /___\  \     /  /
+	  /  ______\  \   /  /
+	 /  /       \  \_/  /
+	/__/         \_____/
+
+
+	This file was made for a Noah Arcouette product.
+	https://github.com/Noah-Arcouette
+
+*/
+
+#define ERROR_C	FG_RGB(221, 0, 6) // error color
+
+#define DEF_C	FG_RGB(240, 240, 240) // default color
+#define B_C 	""   // background color
+
+#define H1_C 	FG_RGB(51, 135, 204) // header 1 color
+#define H2_C 	FG_RGB(84, 112, 255) // header 2 color
+#define H3_C 	FG_RGB(50, 205, 50) // header 3 color
+
+#define HU // header underline
+
+#define CLC_C 	FG_RGB(110, 203, 112) // check list checked
+#define CLU_C 	FG_RGB(51, 135, 204) // check list unchecked
+
+#define UL_C FG_RGB(221, 160, 221) // under line and list color
+
+#define HLB_C 	 BG_RGB(25, 25, 25) // highlight background color
+#define HL_C 	 FG_RGB(208, 160, 208)  // highlight foreground color
+
+#define OP_C  FG_RGB(110, 203, 112)  // operator color
+#define INT_C FG_RGB(51, 135, 204)  // integer color
+#define PAR_C FG_RGB(84, 112, 255)  // parentheses color
+#define STR_C FG_RGB(254, 194, 67) // string color
+
+#define I // italicize
---
+++ styles/tty-support-noadvo.h
@@ -0,0 +1,47 @@
+/*
+	      _____           ___
+	     /  _  \         /  /
+	    /  / \  \       /  /
+	   /  /___\  \     /  /
+	  /  ______\  \   /  /
+	 /  /       \  \_/  /
+	/__/         \_____/
+
+
+	This file was made for a Noah Arcouette product.
+	https://github.com/Noah-Arcouette
+
+	this file is for users running on a TTY session
+
+*/
+
+#define ERROR_C	FG_ANSI(1) // error color
+
+#define DEF_C	FG_ANSI(7) // default color
+#define B_C 	""   // background color
+
+#define H1_C 	FG_ANSI(5) // header 1 color
+#define H2_C 	FG_ANSI(4) // header 2 color
+#define H3_C 	FG_ANSI(6) // header 3 color
+
+// #define HU // header underline
+
+#define CLC_C 	FG_ANSI(6) // check list checked
+#define CLU_C 	FG_ANSI(4) // check list unchecked
+
+#define UL_C FG_ANSI(1) // under line and list color
+
+// #define LI // list indent
+
+#define HLB_C 	 "" // highlight background color
+#define HL_C 	 FG_ANSI(2)  // highlight foreground color
+
+#define OP_C  FG_ANSI(1) // operator color
+#define INT_C FG_ANSI(5)  // integer color
+#define PAR_C FG_ANSI(4)  // parentheses color
+#define STR_C FG_ANSI(3) // string color
+
+#define NAME "TTY-Support"
+#define BY   "Noah Arcouette"
+
+// #define I // italicize
